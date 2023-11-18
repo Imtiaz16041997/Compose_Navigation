1edc0fad8821009a1964f8b85a2aa2c32d11118e
ibnul.imtiaz <imtiazeemel42@gmail.com>
2023-11-18 12:52:27 +0600

    navigate and pass data from one page to another page with function

--- app/src/main/java/com/imtiaz/composenavigation/CustomNav2.kt
+++ app/src/main/java/com/imtiaz/composenavigation/CustomNav2.kt
@@ -8,28 +8,36 @@ import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun CustomNav2(){
     val navController = rememberNavController() 
     
     NavHost(navController = navController, startDestination = "registration"){
-        composable(route = "main"){
-            MainScreen()
+        composable(route = "registration"){
+//            RegistrationScreen(navController)
+            RegistrationScreen{
+                navController.navigate("main/${it}")
+            }
         }
+
 
         composable(route = "login"){
             LoginScreen()
         }
 
-        composable(route = "registration"){
-//            RegistrationScreen(navController)
-            RegistrationScreen{
-                navController.navigate("main")
+        composable(route = "main/{email}", arguments = listOf(
+            navArgument("email"){
+                type = NavType.StringType
             }
+        )){
+            val email = it.arguments!!.getString("email")
+            MainScreen2(email!!)
         }
     }
 
@@ -37,10 +45,10 @@ fun CustomNav2(){
 
 
 @Composable
-fun RegistrationScreen(/*navController: NavController*/onClick: ()-> Unit){
+fun RegistrationScreen(/*navController: NavController*/onClick: (email:String)-> Unit){
     Text(text = "Registration", style = MaterialTheme.typography.headlineLarge,
         modifier = Modifier.clickable {
-            onClick()
+            onClick("it@gmail.com")
         })
 }
 
@@ -50,7 +58,7 @@ fun LoginScreen(){
 }
 
 @Composable
-fun MainScreen(){
-    Text(text = "Main", style = MaterialTheme.typography.headlineLarge)
+fun MainScreen2(email:String){
+    Text(text = "Main$email", style = MaterialTheme.typography.headlineLarge)
 }
 
